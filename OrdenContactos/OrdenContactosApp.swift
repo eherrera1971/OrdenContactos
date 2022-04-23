113dddc0754a13bb9745d03b8f63a7474ef2eb88
Eduardo Herrera <Eduardo@7seeds.cl>
2022-04-22 22:44:49 -0400

    Initial Commit

---
+++ OrdenContactos/OrdenContactosApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrdenContactosApp.swift
+//  OrdenContactos
+//
+//  Created by Eduardo Herrera Barros on 22-04-22.
+//
+
+import SwiftUI
+
+@main
+struct OrdenContactosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
