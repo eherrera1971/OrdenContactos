f08e71e6fb50a096b8eedef0c3e42210d94a1385
Eduardo Herrera <Eduardo@7seeds.cl>
2022-04-28 08:10:13 -0400

    Funciona. Manejo de contactos en código. Buen logro

--- OrdenContactos/ContentView.swift
+++ OrdenContactos/ContentView.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import Contacts
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("Hola")
             .padding()
+            .onAppear {
+                Task.init{
+                await traerTodosContactos()
+                }
+            }
     }
+    
+
+
+func traerTodosContactos() async {
+    let store = CNContactStore()
+    
+    let keys = [CNContactGivenNameKey, CNContactPhoneNumbersKey,CNContactFamilyNameKey] as [CNKeyDescriptor]
+    
+    let request = CNContactFetchRequest(keysToFetch: keys)
+    
+//    var nuevoContacto = CNMutableContact()
+    
+    do {
+        try store.enumerateContacts(with: request, usingBlock: { contacto, resultado in
+            print(contacto.givenName)
+            if contacto.givenName == "Ferran" {
+                guard let nuevoContacto = contacto.mutableCopy() as? CNMutableContact else { return }
+                nuevoContacto.familyName = "Español Amigo"
+                
+                let saveRequest = CNSaveRequest()
+                
+                saveRequest.update(nuevoContacto) //.delete .add
+                
+                do {
+                    try store.execute(saveRequest)
+                } catch {
+                    print("Saving contact failed, error: \(error)")
+                    // Handle the error
+                }
+            }
+        })
+    }
+    catch
+    {
+        print("Error")
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
